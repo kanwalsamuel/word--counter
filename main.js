840b53638a4b04bcdb6c68a0fb0a39e6327be6f3
kanwalsamuel <160218038+kanwalsamuel@users.noreply.github.com>
2024-04-20 04:26:26 +0500

    Add files via upload
    cli based word counter

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green("WELCOME TO WORD COUNTER"));
+const counter = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count "
+    }
+]);
+const words = counter.sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.blueBright(`your sentence word count is ${words.length}`));
